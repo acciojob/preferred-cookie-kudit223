09f5c2b9c55a6d3d6475a59aa0aff7302e270d85
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2026-02-11 17:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const fontsize=document.querySelector('#fontsize');
+const fontcolor=document.querySelector('#fontcolor');
+
+function save(){
+    let fontsizeValue=fontsize.value;
+    let fontcolorValue=fontcolor.value;
+
+    document.cookie=`fontsize=${fontsizeValue}`
+    document.cookie=`fontcolor=${fontcolorValue}`;
+    
+}
+
+function render(){
+    if(document.cookie){
+        let arr=document.cookie.split(';')
+        fontsize.value=arr[0].split('=')[1];
+        fontcolor.value=arr[1].split('=')[1];
+    }
+}
+render()
